ef60728b4ce3825014b7d742d747285608adcd0b
Tim Pickles <tim.pickles@snyk.io>
2024-03-13 11:57:22 +0000

    feat: add initial sbom test command

--- go.mod
+++ go.mod
@@ -15,8 +15,10 @@ require (
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
 	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
 	github.com/jcmturner/gofork v1.7.6 // indirect
@@ -35,9 +37,13 @@ require (
 	github.com/snyk/go-httpauth v0.0.0-20230512081507-800aedece3cb // indirect
 	github.com/spf13/afero v1.9.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/viper v1.13.0 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/oauth2 v0.5.0 // indirect
---
+++ internal/commands/sbomtest/sbomtest_test.go
@@ -0,0 +1,77 @@
+package sbomtest_test
+
+import (
+	_ "embed"
+	"io"
+	"log"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/snyk/go-application-framework/pkg/configuration"
+	"github.com/snyk/go-application-framework/pkg/mocks"
+	"github.com/snyk/go-application-framework/pkg/networking"
+	"github.com/snyk/go-application-framework/pkg/runtimeinfo"
+	"github.com/snyk/go-application-framework/pkg/workflow"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/snyk/cli-extension-sbom/internal/commands/sbomtest"
+)
+
+func TestSBOMTestWorkflow_NoExperimentalFlag(t *testing.T) {
+	mockICTX := createMockICTX(t)
+
+	_, err := sbomtest.TestWorkflow(mockICTX, []workflow.Data{})
+
+	assert.ErrorContains(t, err, "experimental flag not set")
+}
+
+func TestSBOMTestWorkflow_SupplyExperimentalFlag(t *testing.T) {
+	mockICTX := createMockICTX(t)
+	mockICTX.GetConfiguration().Set("experimental", true)
+
+	_, err := sbomtest.TestWorkflow(mockICTX, []workflow.Data{})
+
+	assert.NoError(t, err)
+}
+
+// Helpers
+
+func createMockICTX(t *testing.T) workflow.InvocationContext {
+	t.Helper()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	return mockInvocationContext(t, ctrl, "", nil)
+}
+
+func mockInvocationContext(
+	t *testing.T,
+	ctrl *gomock.Controller,
+	sbomServiceURL string,
+	mockEngine *mocks.MockEngine,
+) workflow.InvocationContext {
+	t.Helper()
+
+	mockLogger := log.New(io.Discard, "", 0)
+
+	mockConfig := configuration.New()
+	mockConfig.Set(configuration.AUTHENTICATION_TOKEN, "<SOME API TOKEN>")
+	mockConfig.Set(configuration.ORGANIZATION, "6277734c-fc84-4c74-9662-33d46ec66c53")
+	mockConfig.Set(configuration.API_URL, sbomServiceURL)
+	mockConfig.Set("format", "cyclonedx1.4+json")
+	mockConfig.Set("name", "goof")
+	mockConfig.Set("version", "0.0.0")
+
+	mockRuntimeInfo := runtimeinfo.New(
+		runtimeinfo.WithName("test-app"),
+		runtimeinfo.WithVersion("1.2.3"))
+
+	ictx := mocks.NewMockInvocationContext(ctrl)
+	ictx.EXPECT().GetConfiguration().Return(mockConfig).AnyTimes()
+	ictx.EXPECT().GetEngine().Return(mockEngine).AnyTimes()
+	ictx.EXPECT().GetNetworkAccess().Return(networking.NewNetworkAccess(mockConfig)).AnyTimes()
+	ictx.EXPECT().GetLogger().Return(mockLogger).AnyTimes()
+	ictx.EXPECT().GetRuntimeInfo().Return(mockRuntimeInfo).AnyTimes()
+
+	return ictx
+}
