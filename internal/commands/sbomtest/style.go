5a51b6a8982983ed27392613723c1515a7548974
Daniel Ekelund <daniel.ekelund@snyk.io>
2024-04-04 09:38:58 +0200

    fix: cleanup lipgloss styles

--- internal/commands/sbomtest/style.go
+++ internal/commands/sbomtest/style.go
@@ -8,71 +8,36 @@ import (
 	"github.com/snyk/cli-extension-sbom/internal/snykclient"
 )
 
-type Mode int
+var (
+	SectionStyle = lipgloss.NewStyle().Bold(true)
 
-/*
-const (
-	JSONOutput Mode = iota
-	HumanReadable
-	HumanReadableWithDeps
-	HumanReadableWithDepsAndPaths
+	red     = lipgloss.Color("1")
+	yellow  = lipgloss.Color("3")
+	magenta = lipgloss.Color("5")
+
+	noColor = lipgloss.NoColor{} // Usually white or black
+
+	// Severity styles renders severities in bold combined with it's color.
+	severityStyle = lipgloss.NewStyle().Bold(true)
+
+	lowStyle      = severityStyle.Copy().Foreground(noColor)
+	mediumStyle   = severityStyle.Copy().Foreground(yellow)
+	highStyle     = severityStyle.Copy().Foreground(red)
+	criticalStyle = severityStyle.Copy().Foreground(magenta)
 )
-
-func GetPrintMode(printJSON, printDeps, printPaths bool) Mode {
-	switch {
-	case printJSON:
-		return JSONOutput
-	case printDeps && printPaths:
-		return HumanReadableWithDepsAndPaths
-	case printDeps:
-		return HumanReadableWithDeps
-	default:
-		return HumanReadable
-	}
-}
-*/
-
-type Result struct {
-	Doc      any
-	MIMEType string
-}
-
-// SectionStyle renders blue and bold.
-var SectionStyle = lipgloss.NewStyle().
-	Bold(true)
-
-// LowStyle renders white and bold.
-var LowStyle = lipgloss.NewStyle().
-	Bold(true).
-	Foreground(lipgloss.NoColor{}) // White
-
-// MediumStyle renders yellow and bold.
-var MediumStyle = lipgloss.NewStyle().
-	Bold(true).
-	Foreground(lipgloss.Color("3")) // Yellow
-
-// HighStyle renders red and bold.
-var HighStyle = lipgloss.NewStyle().
-	Bold(true).
-	Foreground(lipgloss.Color("1")) // Red
-
-// CriticalStyle renders magenta and bold.
-var CriticalStyle = lipgloss.NewStyle().
-	Bold(true).
-	Foreground(lipgloss.Color("5")) // Magenta
 
 func RenderTitle(severity snykclient.SeverityLevel, desc string) string {
 	title := fmt.Sprintf("✗ [%s] %s", severity, desc)
 
 	switch severity {
 	case snykclient.LowSeverity:
-		return LowStyle.Render(title)
+		return lowStyle.Render(title)
 	case snykclient.MediumSeverity:
-		return MediumStyle.Render(title)
+		return mediumStyle.Render(title)
 	case snykclient.HighSeverity:
-		return HighStyle.Render(title)
+		return highStyle.Render(title)
 	case snykclient.CriticalSeverity:
-		return CriticalStyle.Render(title)
+		return criticalStyle.Render(title)
 	}
 
 	return title
