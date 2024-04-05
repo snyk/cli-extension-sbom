8f1b11b8f550d603c6ab070c5cc3bf16ca542265
Daniel Ekelund <daniel.ekelund@snyk.io>
2024-04-05 13:31:12 +0200

    refactor: move unit test to it's own file - part 2

--- internal/commands/sbomtest/presenter_test.go
+++ internal/commands/sbomtest/presenter_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/snyk/cli-extension-sbom/internal/commands/sbomtest"
-	"github.com/snyk/cli-extension-sbom/internal/snykclient"
 )
 
 var snapshotter = cupaloy.New(cupaloy.SnapshotSubdirectory("testdata/snapshots"))
@@ -39,39 +38,4 @@ func TestPresenter_JSON(t *testing.T) {
 
 	assert.Equal(t, "application/json", data[0].GetContentType())
 	snapshotter.SnapshotT(t, data[0].GetPayload())
-}
-
-func TestSortVulns(t *testing.T) {
-	vulns := map[string]snykclient.VulnerabilityResource{
-		"0": {
-			ID:            "0",
-			SeverityLevel: snykclient.MediumSeverity,
-		},
-		"1": {
-			ID:            "1",
-			SeverityLevel: snykclient.CriticalSeverity,
-		},
-		"2": {
-			ID:            "2",
-			SeverityLevel: snykclient.LowSeverity,
-		},
-		"3": {
-			ID:            "3",
-			SeverityLevel: snykclient.HighSeverity,
-		},
-	}
-
-	result := sbomtest.SortVulns(vulns)
-
-	require.Equal(t, "2", result[0].ID)
-	require.Equal(t, snykclient.LowSeverity, result[0].SeverityLevel)
-
-	require.Equal(t, "0", result[1].ID)
-	require.Equal(t, snykclient.MediumSeverity, result[1].SeverityLevel)
-
-	require.Equal(t, "3", result[2].ID)
-	require.Equal(t, snykclient.HighSeverity, result[2].SeverityLevel)
-
-	require.Equal(t, "1", result[3].ID)
-	require.Equal(t, snykclient.CriticalSeverity, result[3].SeverityLevel)
 }
