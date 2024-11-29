cd634b0deb26cb30d83f756c16fb92fbb12dcaa7
Tim Pickles <tim.pickles@snyk.io>
2024-11-29 11:44:43 +0000

    feat: add support for sbom monitor command

---
+++ internal/commands/sbommonitor/sbommonitor.go
@@ -0,0 +1,84 @@
+package sbommonitor
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/snyk/go-application-framework/pkg/configuration"
+	"github.com/snyk/go-application-framework/pkg/workflow"
+
+	"github.com/snyk/cli-extension-sbom/internal/errors"
+	"github.com/snyk/cli-extension-sbom/internal/flags"
+	"github.com/snyk/cli-extension-sbom/internal/sbom"
+	"github.com/snyk/cli-extension-sbom/internal/snykclient"
+)
+
+var WorkflowID = workflow.NewWorkflowIdentifier("sbom.monitor")
+
+func RegisterWorkflows(e workflow.Engine) error {
+	sbomFlagset := flags.GetSBOMMonitorFlagSet()
+
+	c := workflow.ConfigurationOptionsFromFlagset(sbomFlagset)
+
+	if _, err := e.Register(WorkflowID, c, MonitorWorkflow); err != nil {
+		return fmt.Errorf("error while registering %s workflow: %w", WorkflowID, err)
+	}
+	return nil
+}
+
+func MonitorWorkflow(
+	ictx workflow.InvocationContext,
+	_ []workflow.Data,
+) ([]workflow.Data, error) {
+	config := ictx.GetConfiguration()
+	logger := ictx.GetLogger()
+	experimental := config.GetBool(flags.FlagExperimental)
+	filename := config.GetString(flags.FlagFile)
+	errFactory := errors.NewErrorFactory(logger)
+	ctx := context.Background()
+
+	logger.Println("SBOM Monitor workflow start")
+
+	// As this is an experimental feature, we only want to continue if the experimental flag is set
+	if !experimental {
+		return nil, errFactory.NewMissingExperimentalFlagError()
+	}
+
+	logger.Println("Getting preferred organization ID")
+
+	orgID := config.GetString(configuration.ORGANIZATION)
+	if orgID == "" {
+		return nil, errFactory.NewEmptyOrgError()
+	}
+
+	if filename == "" {
+		return nil, errFactory.NewMissingFilenameFlagError()
+	}
+
+	logger.Println("Target SBOM document:", filename)
+
+	bts, err := sbom.ReadSBOMFile(filename, errFactory)
+	if err != nil {
+		return nil, err
+	}
+
+	client := snykclient.NewSnykClient(
+		ictx.GetNetworkAccess().GetHttpClient(),
+		config.GetString(configuration.API_URL),
+		orgID,
+	)
+	sbomMonitor, err := client.CreateSBOMMonitor(ctx, bts, "", errFactory)
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Printf("Created SBOM monitor (ID %s)\n", sbomMonitor.ID)
+
+	data := "SBOM Monitor Success!"
+	return []workflow.Data{workflowData([]byte(data), "text/plain")}, err
+}
+
+func workflowData(data []byte, contentType string) workflow.Data {
+	id := workflow.NewTypeIdentifier(WorkflowID, "sbom.monitor")
+	return workflow.NewDataFromInput(nil, id, contentType, data)
+}
