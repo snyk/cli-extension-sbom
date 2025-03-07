2b6d77c0b4ef13361b61e1a051c745abc9440d6c
Maximilian Combüchen <max.combuchen@snyk.io>
2025-03-07 14:31:39 +0100

    feat: add sbom monitor command
    Co-authored-by: Tim Pickles <tim.pickles@snyk.io>

---
+++ internal/commands/sbommonitor/sbommonitor.go
@@ -0,0 +1,63 @@
+package sbommonitor
+
+import (
+	"fmt"
+
+	"github.com/snyk/go-application-framework/pkg/configuration"
+	"github.com/snyk/go-application-framework/pkg/workflow"
+
+	"github.com/snyk/cli-extension-sbom/internal/errors"
+	"github.com/snyk/cli-extension-sbom/internal/flags"
+)
+
+var WorkflowID = workflow.NewWorkflowIdentifier("sbom.monitor")
+var WorkflowDataID = workflow.NewTypeIdentifier(WorkflowID, "sbom.monitor")
+
+func RegisterWorkflows(e workflow.Engine) error {
+	sbomFlagset := flags.GetSBOMMonitorFlagSet()
+
+	c := workflow.ConfigurationOptionsFromFlagset(sbomFlagset)
+
+	if _, err := e.Register(WorkflowID, c, MonitorWorkflow); err != nil {
+		return fmt.Errorf("error while registering %s workflow: %w", WorkflowID, err)
+	}
+	return nil
+}
+
+func MonitorWorkflow(
+	ictx workflow.InvocationContext,
+	_ []workflow.Data,
+) ([]workflow.Data, error) {
+	config := ictx.GetConfiguration()
+	logger := ictx.GetLogger()
+	experimental := config.GetBool(flags.FlagExperimental)
+	filename := config.GetString(flags.FlagFile)
+	errFactory := errors.NewErrorFactory(logger)
+
+	logger.Println("SBOM Monitor workflow start")
+
+	// As this is an experimental feature, we only want to continue if the experimental flag is set
+	if !experimental {
+		return nil, errFactory.NewMissingExperimentalFlagError()
+	}
+
+	logger.Println("Getting preferred organization ID")
+
+	orgID := config.GetString(configuration.ORGANIZATION)
+	if orgID == "" {
+		return nil, errFactory.NewEmptyOrgError()
+	}
+
+	if filename == "" {
+		return nil, errFactory.NewMissingFilenameFlagError()
+	}
+
+	logger.Println("Target SBOM document:", filename)
+
+	data := "SBOM Monitor Success!"
+	return []workflow.Data{workflowData([]byte(data), "text/plain")}, nil
+}
+
+func workflowData(data []byte, contentType string) workflow.Data {
+	return workflow.NewData(WorkflowDataID, contentType, data)
+}
