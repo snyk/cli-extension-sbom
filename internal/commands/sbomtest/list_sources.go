bdc7801071d1534739a4033dd6319f5567f68678
Kris <6713429+klesniewski@users.noreply.github.com>
2025-04-15 17:10:40 +0200

    feat: add method to list source files for uploading (#137)
    * feat: add method to list sources to upload
    
    * chore(lint): fix linting errors
    
    ---------
    
    Co-authored-by: Thomas Schafer <thomasschafer97@gmail.com>

---
+++ internal/commands/sbomtest/list_sources.go
@@ -0,0 +1,18 @@
+package sbomtest
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/snyk/go-application-framework/pkg/utils"
+)
+
+// Return a channel that notifies each file in the path that doesn't match the filter rules.
+func GetFilesForPath(path string, logger *zerolog.Logger, max_threads int) (<-chan string, error) {
+	filter := utils.NewFileFilter(path, logger, utils.WithThreadNumber(max_threads))
+	rules, err := filter.GetRules([]string{".gitignore", ".dcignore", ".snyk"})
+	if err != nil {
+		return nil, err
+	}
+
+	results := filter.GetFilteredFiles(filter.GetAllFiles(), rules)
+	return results, nil
+}
